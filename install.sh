638b3a6d94c38bc2ba97a758ad73e5529d28c111
alfredtofu <alfredtofu@alfredtofu-Desktop.(none)>
2013-05-13 21:23:42 +0800

    fix use of nerd tree

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #/bin/sh
 
-#if [ ! -e $HOME/.vim/bundle/vundle ]; then
-    #git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-#fi
+if [ ! -e $HOME/.vim/bundle/vundle ]; then
+   git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
 
-#system_shell=$SHELL
-#export SHELL="/bin/sh"
-#vim +BundleInstall! +BundleClean +qall
-#export SHELL=$system_shell
+system_shell=$SHELL
+export SHELL="/bin/sh"
+vim +BundleInstall! +BundleClean +qall
+export SHELL=$system_shell
 
 rm ~/.vim/bundle/c.vim/c-support/templates/Templates
 ln -s ~/.vim/Templates ~/.vim/bundle/c.vim/c-support/templates/
